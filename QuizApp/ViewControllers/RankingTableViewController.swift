b9fc332fb2743c3a9a94baaffdd57363f5efd7ae
Blaire Crits-Christoph <bcritschristoph@gmail.com>
2021-05-13 17:09:02 -0400

    rankings table (admin view)

--- QuizApp/ViewControllers/RankingTableViewController.swift
+++ QuizApp/ViewControllers/RankingTableViewController.swift
@@ -9,24 +9,9 @@ import UIKit
 
 class RankingTableViewController: UITableViewController {
 
-    @IBOutlet weak var table: UITableView!
-    
-    var categories : [Category] = []
-    
-    var rankedUsersByCategory : [String : [User]] = [:]
-
-    
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        table.delegate = self
-        table.dataSource = self
-        for category in categories {
-            let sortedUsers = sortUsersAccordingToRank(DBHelper.inst.getUsersWithScoresInCategory(category), inCategory: category)
-            rankedUsersByCategory.updateValue(sortedUsers, forKey: category.name)
-            
-        }
-        
+
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -38,7 +23,7 @@ class RankingTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return categories.count
+        return 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -46,30 +31,15 @@ class RankingTableViewController: UITableViewController {
         return 0
     }
 
-    
+    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! RankTableViewCell
-        let categoryName = categories[indexPath.section].name
-        cell.rankingLabel.text = String(indexPath.row + 1)
-        cell.usernameLabel.text = rankedUsersByCategory[categoryName][indexPath.row].username
-        cell.scoreLabel.text = rankedUsersByCategory[categoryName][indexPath.row].avgScores[categoryName]
-        
-        //cell.rankingLabel.text =
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
 
         return cell
     }
-    
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return categories[section].name
-    }
-    
-    func sortUsersAccordingToRank(_ users: [User], inCategory category: Category) -> [User] {
-        let categoryName = category.name
-        users.sort {
-            return ($0.rankings[categoryName] < $1.rankings[categoryName])
-        }
-    }
-    
+    */
 
     /*
     // Override to support conditional editing of the table view.
